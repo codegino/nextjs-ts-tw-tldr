cea430c7c20a10f37e178918d467f87d0d5085d9
codegino <carloginocatapang@gmail.com>
2025-03-25 14:12:22 +0100

    feat: Display package versions on the index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+// import package.json as json
+import json from '../../package.json';
+
 function IndexPage() {
   return (
     <main>
       <h1 className="mb-4">
         Hello there, <span className="animate-bounce inline-block">👋</span>
       </h1>
-      <h2>This page is built with Next.js, TypeScript, and Tailwind CSS.</h2>
+      <h2>This page is built with:</h2>
+      <p>NextJS: v{json.dependencies.next}</p>
+      <p>React: v{json.dependencies.react}</p>
+      <p>Typescript: v{json.devDependencies.typescript}</p>
+      <p>Tailwind: v{json.devDependencies.tailwindcss}</p>
+      <p>ESLint: v{json.devDependencies.eslint}</p>
+      <p>Prettier: v{json.devDependencies.prettier}</p>
     </main>
   );
 }
