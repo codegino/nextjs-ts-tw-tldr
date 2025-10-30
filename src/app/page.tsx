5280f793f15e5bbc0693a5d90a61c2cc33324853
codegino <carloginocatapang@gmail.com>
2025-10-30 13:34:43 +0100

    Refactor package version display to use a list format for better readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,29 @@ function IndexPage() {
         Hello there, <span className="animate-bounce inline-block">👋</span>
       </h1>
       <h2>This page is built with:</h2>
-      <p>
-        NextJS: v<span className="italic">{json.dependencies.next}</span>
-      </p>
-      <p>
-        React: v<span className="italic">{json.dependencies.react}</span>
-      </p>
-      <p>
-        Typescript: v
-        <span className="italic">{json.devDependencies.typescript}</span>
-      </p>
-      <p>
-        Tailwind: v
-        <span className="italic">{json.devDependencies.tailwindcss}</span>
-      </p>
-      <p>
-        ESLint: v<span className="italic">{json.devDependencies.eslint}</span>
-      </p>
-      <p>
-        Prettier: v
-        <span className="italic">{json.devDependencies.prettier}</span>
-      </p>
+      <ul className="list-disc list-outside ml-6">
+        <li>
+          NextJS: v<span className="italic">{json.dependencies.next}</span>
+        </li>
+        <li>
+          React: v<span className="italic">{json.dependencies.react}</span>
+        </li>
+        <li>
+          Typescript: v
+          <span className="italic">{json.devDependencies.typescript}</span>
+        </li>
+        <li>
+          Tailwind: v
+          <span className="italic">{json.devDependencies.tailwindcss}</span>
+        </li>
+        <li>
+          ESLint: v<span className="italic">{json.devDependencies.eslint}</span>
+        </li>
+        <li>
+          Prettier: v
+          <span className="italic">{json.devDependencies.prettier}</span>
+        </li>
+      </ul>
     </main>
   );
 }
